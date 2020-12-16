dd71b36a7cbc74335b2c869252723272b00a09af
trentyy <trent.y.yang@gmail.com>
2020-12-17 05:14:11 +0800

    read data from epa

--- cwb_read.py
+++ cwb_read.py
@@ -11,7 +11,7 @@ LOG = "cwb_read.log"
 counter = 0
 udtCWB = 10 # update time for cwb: 10 min 
 wdCWB = True # writing data to cwb
-myDict = dict()
+
 
 with open("AQDC.json", 'r') as f:
     jdata = json.load(f)
@@ -21,8 +21,10 @@ USER = jdata['user']
 PW = jdata['pw']
 DB = jdata['db']
 
-url = jdata['url']
-params = jdata['params']
+url_cwb = jdata['cwb']['url']
+params_cwb = jdata['cwb']['params']
+url_epa = jdata['airtw_epa']['url']
+params_epa = jdata['airtw_epa']['params']
 
 # here is the name of data which you want to store 
 needed_id = ['lat', 'lon', 'locationName', 
@@ -32,11 +34,33 @@ needed_id = ['lat', 'lon', 'locationName',
 
 # store request url, include cwb key, do not upload!
 # curl -X GET "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=YOURKEY&stationId=467490" -H  "accept: application/json"
-
-
-def getDataCWB(myDict, DEBUG = False):
-    # get data from cwb
+def getDataEPA(url, params, DEBUG = False):
+    myDict = dict()
+    # get data from airtw.epa
     data = requests.get(url, params=params)
+    data = data.text
+    # use json.loads() to parse data from string-version, will become type: dict
+    jdata = json.loads(data)
+
+    tmpList = jdata['records']
+    # parse data
+    for item in tmpList:
+        if item['ItemEngName'] == 'PM2.5':
+            myDict['PM2_5'] = item['Concentration']
+            myDict['time'] = item['MonitorDate']
+        elif item['ItemEngName'] == 'PM10':
+            myDict['PM10'] = item['Concentration']
+
+    return myDict
+
+
+def getDataCWB(url, params, DEBUG = False):
+    myDict = dict()
+    # get data from cwb
+
+    data = requests.get(url, params=params, verify=False)
+
+    
     data = data.text
 
     # use json.loads() to parse data from string-version, will become type: dict
@@ -69,6 +93,7 @@ def getDataCWB(myDict, DEBUG = False):
     
 
     myDict['HUMD'] = float(myDict['HUMD']) * 100
+    return myDict
 
 
 while True:
@@ -76,30 +101,33 @@ while True:
     counter += 1  
     if (counter >= udtCWB or wdCWB):
         f = open(LOG, 'a')
-        getDataCWB(myDict)
+        cwb_dict = getDataCWB(url_cwb, params_cwb)
+        print(cwb_dict)
+        time.sleep(10)
+        epa_dict = getDataEPA(url_epa, params_epa)
+        
+        print(epa_dict)
         try:
-            sql = "INSERT IGNORE INTO cwb_Taichung( obsTime, " + \
+            sql_cwb = "INSERT IGNORE INTO cwb_Taichung( obsTime, " + \
                 "lat, lon, locationName," + \
                 "TEMP, HUMD, PRES," +\
                 "H_F10, H_UVI, Weather, CITY, TOWN)" + \
-                f"VALUES(\'{myDict['obsTime']}\', {myDict['lat']}, {myDict['lon']}, \'{myDict['locationName']}\', " + \
-                f"{myDict['TEMP']}, {myDict['HUMD']}, {myDict['PRES']}, {myDict['H_F10']}," + \
-                f"{myDict['H_UVI']}, \'{myDict['Weather']}\', \'{myDict['CITY']}\', \'{myDict['TOWN']}\'" + \
+                f"VALUES(\'{cwb_dict['obsTime']}\', {cwb_dict['lat']}, {cwb_dict['lon']}, \'{cwb_dict['locationName']}\', " + \
+                f"{cwb_dict['TEMP']}, {cwb_dict['HUMD']}, {cwb_dict['PRES']}, {cwb_dict['H_F10']}," + \
+                f"{cwb_dict['H_UVI']}, \'{cwb_dict['Weather']}\', \'{cwb_dict['CITY']}\', \'{cwb_dict['TOWN']}\'" + \
                 ");"
-        except Exception as e:
-            f.write(str(datetime.now()) + '\tsql string exception: ')
-            f.write(str(e))
-            f.write("\n sql= " + sql + '\n')
-            f.close()
-            continue
-        try:
+            sql_epa = "INSERT IGNORE INTO epa_Taichung( time, " + \
+                "PM2_5, PM10)" + \
+                f"VALUES(\'{epa_dict['time']}\', '\{epa_dict['PM2_5']}\', \'{epa_dict['PM10']}\');"
             db = pymysql.connect(host=HOST, user=USER, password=PW, db=DB,
                              cursorclass=pymysql.cursors.DictCursor)
-            db.cursor().execute(sql)
+            db.cursor().execute(sql_cwb)
+            db.commit()
+            db.cursor().execute(sql_epa)
             db.commit()
             db.close()
 
-            f.write(str(datetime.now()) + '\tSuccess insert, obsTime: ' + str(myDict['obsTime']) + '\n')
+            f.write(str(datetime.now()) + '\tSuccess insert, obsTime: ' + str(cwb_dict['obsTime']) + '\n')
             f.close()
 
             counter = 0
@@ -108,7 +136,7 @@ while True:
             f.write(str(datetime.now()) + '\tException: ')
             f.write(str(e))
             f.write("\n sql= " + sql + '\n')
-            f.write(f"myDict: {myDict}" + '\n')
+            f.write(f"myDict: {cwb_dict}" + '\n')
             f.close()
             counter = 9
             
